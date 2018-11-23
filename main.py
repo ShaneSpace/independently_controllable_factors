2acd2644b9e063ef265d2b7a5b1a41b005692810
Chris Grimm <cgrimm1994@gmail.com>
2018-11-22 20:05:29 -0500

    fixed critical bug in the architecture where the environment wasnt producing transitions correctly. also changed the model to not use REINFORCE

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 from network import IndepFeatureLearner
-from visualizations import visualize_correlation, visualize_policies
+from visualizations import visualize_correlation, visualize_policies, visualize_autoencoder
 from gridworld import SimpleGridworld
 import numpy as np
 import itertools
+import utils, replay_buffer
 import os
 
 env = SimpleGridworld()
 dummy_env = SimpleGridworld()
 net = IndepFeatureLearner()
-visualization_freq = 1000
+buffer = replay_buffer.ReplayBuffer(10000)
 
-def run_training_step(net: IndepFeatureLearner, env : SimpleGridworld):
-    s = env.get_observation(env.position)
-    actions = net.get_actions([s]) # [1, num_factors]
-    sp = []
-    for i in range(net.num_factors):
-        dummy_env.set_position(env.get_position())
-        sp.append(np.reshape(dummy_env.step(actions[0,i]), [12, 12, 1, 1]))
-    sp = np.concatenate(sp, axis=-1)
-    return net.train_step([s], actions, [sp])
+visualization_freq = 1000
+batch_size = 64
+
+def run_training_step(buffer : replay_buffer.ReplayBuffer, net : IndepFeatureLearner, env : SimpleGridworld):
+    positions, _, _, _, _ = buffer.sample(batch_size)
+    s_list = []
+    sp_list = []
+    action_list = []
+    for pos in positions:
+        s = dummy_env.get_observation(pos)
+        #actions = net.([s]) # [1, num_factors]
+        sp = []
+        for i in range(net.num_actions):
+            dummy_env.set_position(pos)
+            sp.append(np.reshape(dummy_env.step(i), [12, 12, 1, 1]))
+        sp = np.concatenate(sp, axis=-1)
+        s_list.append(s)
+        sp_list.append(sp)
+    return net.train_step(s_list, sp_list)
 
 
 for i in itertools.count():
     s = env.get_observation()
+    buffer.append(env.get_position(), None, None, None, None)
     a = np.random.randint(0, 4)
     sp = env.step(a)
-    recon_loss, encoder_loss, pi_loss = run_training_step(net, env)
+
+    if buffer.length() < 1000:
+        continue
+
+    recon_loss, encoder_loss, pi_loss = run_training_step(buffer, net, env)
     print(f'{i}: recon_loss: {recon_loss} encoder_loss: {encoder_loss} pi_loss: {pi_loss}')
     if i % visualization_freq == 0:
         visualize_policies(f'./vis/policies/{i}_', net, env)
         visualize_correlation(f'./vis/correlation/{i}_', net, env)
+        visualize_autoencoder(f'./vis/autoencoder/', net, env)
 
--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,5 @@
 import cv2, numpy as np, os
+from utils import horz_stack_images
 from network import IndepFeatureLearner
 from gridworld import SimpleGridworld
 
@@ -10,7 +11,6 @@ def get_all_states(env : SimpleGridworld):
 def visualize_correlation(file_path : str, network : IndepFeatureLearner, env : SimpleGridworld):
     all_positions, all_observations = get_all_states(env)
     all_fs = network.get_f(all_observations) # [bs, num_factors]
-
     images = []
     for i in range(network.num_factors):
         all_f_i = all_fs[:, i]
@@ -33,4 +33,16 @@ def visualize_policies(file_path : str, network : IndepFeatureLearner, env : Sim
     canvas = cv2.applyColorMap(canvas, cv2.COLORMAP_JET)
     canvas = cv2.resize(canvas, (400, 400), interpolation=cv2.INTER_NEAREST)
     cv2.imwrite(file_path + f'policy_averages.png', canvas)
+
+def visualize_autoencoder(file_path : str, network : IndepFeatureLearner, env : SimpleGridworld):
+    all_positions, all_observation = get_all_states(env)
+    all_recons = network.get_recon(all_observation)
+    print()
+    for recon, position, obs in zip(all_recons, all_positions, all_observation):
+        recon = 255*cv2.resize(recon, (400, 400), interpolation=cv2.INTER_NEAREST)
+        recon = np.tile(np.reshape(recon, [400, 400, 1]), [1,1,3])
+        orig = 255*cv2.resize(obs, (400, 400), interpolation=cv2.INTER_NEAREST)
+        orig = np.tile(np.reshape(orig, [400, 400, 1]), [1,1,3])
+        stacked = horz_stack_images(orig, recon, background_color=(255, 0, 0))
+        cv2.imwrite(file_path+f'({position[1]}_{position[0]}.png', stacked)
 
